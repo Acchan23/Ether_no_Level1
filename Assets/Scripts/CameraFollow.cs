7ca919bea133081b70b9f99596d23708b58e6ee1
Alejonicholls <alejandronicholls10@gmail.com>
2024-12-20 16:16:06 -0500

    Add Camera follow and adjust

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class CameraFollow : MonoBehaviour
 {
     public Transform target;          // The player or target to follow
-    public Vector3 offset = new Vector3(0, 10, -10); // Offset of the camera relative to the target
+    public Vector3 offset = new Vector3(0, 15, -5); // Offset of the camera relative to the target
     public float smoothSpeed = 0.125f; // Speed of the camera's smooth movement
 
     private void LateUpdate()
