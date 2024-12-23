b766f35c79d4cff6c1a4c8f3d3dc251ff7cb0da4
Alejonicholls <alejandronicholls10@gmail.com>
2024-12-23 17:14:07 -0500

    ajuste codigo camara y anexo enemigo

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -5,23 +5,39 @@ using UnityEngine;
 public class CameraFollow : MonoBehaviour
 {
     public Transform target;          // The player or target to follow
-    public Vector3 offset = new Vector3(0, 9, -10); // Offset of the camera relative to the target
+    public Vector3 offset = new Vector3(0, 5, -8); // Offset of the camera relative to the target
     public float smoothSpeed = 0.135f; 
 
-    private void LateUpdate()
-{
-    if (target == null)
+     void Start()
     {
-        Debug.LogWarning("No target assigned for the camera to follow!");
-        return;
+       
+        transform.position = target.position + offset;
+        
+        if (offset == Vector3.zero)
+        {
+            offset = transform.position - target.position;
+        }
     }
 
-    // Calculate the desired position
-    Vector3 desiredPosition = target.position + offset;
+    private void LateUpdate()
+    {
+          if (target != null)
+        {
+            transform.position = target.position + offset;
+        }
+        
+        if (target == null)
+        {
+            Debug.LogWarning("No target assigned for the camera to follow!");
+            return;
+        }
 
-    Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
-    transform.position = smoothedPosition;
-    transform.LookAt(target);
-}
+        // Calculate the desired position
+        Vector3 desiredPosition = target.position + offset;
+
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
+        transform.position = smoothedPosition;
+        transform.LookAt(target);
+    }
 
 }
